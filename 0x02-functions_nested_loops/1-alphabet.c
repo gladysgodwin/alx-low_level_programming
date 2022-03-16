fada487f667b16f397b11095b43a625174a6c2ed
Gladys Godwin <gladysgodwin79@gmail.com>
2022-03-16 15:12:20 +0100

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * main - Print alphabet
+ * print_alphabet - check the code
  *
  * Return = Always 0 (Success)
  */
 void print_alphabet(void)
 
 {
-char alp = 'a';
+char ch;
 
-while (alp <= 'z')
+_putchar ("Alphabets from a - z are: \n");
+for (ch = 'a'; ch <= 'z'; ch++)
 
 {
 
-_putchar (alp);
-alp++;
+_putchar ("%c\n", ch);
 
 }
 
-_putchar ('\n');
+return 0;
 
 }
