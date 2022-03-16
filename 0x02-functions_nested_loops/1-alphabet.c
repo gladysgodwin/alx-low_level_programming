22167427696a7866ad19c509345577db490616d6
Gladys Godwin <gladysgodwin79@gmail.com>
2022-03-16 14:48:20 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Print alphabet
+ *
+ * Return = Always 0 (Success)
+ */
+void print_alphabet(void)
+
+{
+char alp = 'a';
+
+while (alp <= 'z')
+
+{
+
+_putchar (alp);
+alp++;
+
+}
+
+_putchar ('\n');
+
+return (0);
+
+}
