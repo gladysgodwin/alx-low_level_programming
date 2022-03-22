c8370f1bf9ccc97fdafacd4a0bcbf051885fe277
root <gladysgodwin79@gmail.com>
2022-03-22 13:01:09 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print characters of a string followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+
+		i++;
+
+	}
+	putchar('\n');
+
+}
